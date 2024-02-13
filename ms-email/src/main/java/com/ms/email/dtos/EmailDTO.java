d72d35ef371fe9ad093333f7280fce48a62b1a9d
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-02-12 22:23:00 -0300

    Update EmailDTO.java
    - Criando os getter e setter para retorno e preenchimento de variaveis;
    - Lombok não funcionou;

--- ms-email/src/main/java/com/ms/email/dtos/EmailDTO.java
+++ ms-email/src/main/java/com/ms/email/dtos/EmailDTO.java
@@ -23,4 +23,46 @@ public class EmailDTO {
 	
 	@NotBlank
 	private String textString;
+
+	public String getOwnerRef() {
+		return ownerRef;
+	}
+
+	public void setOwnerRef(String ownerRef) {
+		this.ownerRef = ownerRef;
+	}
+
+	public String getEmailFrom() {
+		return emailFrom;
+	}
+
+	public void setEmailFrom(String emailFrom) {
+		this.emailFrom = emailFrom;
+	}
+
+	public String getEmailTo() {
+		return emailTo;
+	}
+
+	public void setEmailTo(String emailTo) {
+		this.emailTo = emailTo;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getTextString() {
+		return textString;
+	}
+
+	public void setTextString(String textString) {
+		this.textString = textString;
+	}
+	
+	
 }
