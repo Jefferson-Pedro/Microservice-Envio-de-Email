164d9e13c2b981ec79500551a310978879952909
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-02-12 22:23:37 -0300

    Update Email.java
    - O lombok falhou, estou criando getters e setters para manipulação dos atributos;

--- ms-email/src/main/java/com/ms/email/models/Email.java
+++ ms-email/src/main/java/com/ms/email/models/Email.java
@@ -33,10 +33,80 @@ public class Email  implements Serializable{
 	private String subject;
 	
 	@Column(columnDefinition = "TEXT")
-	private String textString;
+	private String text;
 	
 	private LocalDateTime sendDateEmaiL;
 	
 	private StatusEmail statusEmail;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getOwnerRef() {
+		return ownerRef;
+	}
+
+	public void setOwnerRef(String ownerRef) {
+		this.ownerRef = ownerRef;
+	}
+
+	public String getEmailFrom() {
+		return emailFrom;
+	}
+
+	public void setEmailFrom(String emailFrom) {
+		this.emailFrom = emailFrom;
+	}
+
+	public String getEmailTo() {
+		return emailTo;
+	}
+
+	public void setEmailTo(String emailTo) {
+		this.emailTo = emailTo;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setTextString(String text) {
+		this.text = text;
+	}
+
+	public LocalDateTime getSendDateEmaiL() {
+		return sendDateEmaiL;
+	}
+
+	public void setSendDateEmaiL(LocalDateTime sendDateEmaiL) {
+		this.sendDateEmaiL = sendDateEmaiL;
+	}
+
+	public StatusEmail getStatusEmail() {
+		return statusEmail;
+	}
+
+	public void setStatusEmail(StatusEmail statusEmail) {
+		this.statusEmail = statusEmail;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+	
 	
 }
