347fb20bb6fed7b354960a308d0a4d2979b76d53
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-02-12 23:24:30 -0300

    Update EmailController.java

--- ms-email/src/main/java/com/ms/email/controllers/EmailController.java
+++ ms-email/src/main/java/com/ms/email/controllers/EmailController.java
@@ -1,6 +1,7 @@
 package com.ms.email.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,7 +23,7 @@ public class EmailController {
 	public ResponseEntity<Email> sendingEmail(@RequestBody @Valid EmailDTO emailDTO){
 		
 		Email email = service.sendEmail(emailDTO);
-		return ResponseEntity.ok(email);
+		return new ResponseEntity<>(email, HttpStatus.CREATED);
 	}
 	
 }
