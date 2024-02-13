3a5d6a73aa1e886a439c090cd87ee7117e6ff12a
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-02-13 12:19:35 -0300

    Atualização

--- ms-email/src/main/java/com/ms/email/consumers/EmailConsumer.java
+++ ms-email/src/main/java/com/ms/email/consumers/EmailConsumer.java
@@ -1,7 +1,6 @@
 package com.ms.email.consumers;
 
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
--- ms-email/src/main/java/com/ms/email/models/Email.java
+++ ms-email/src/main/java/com/ms/email/models/Email.java
@@ -20,7 +20,7 @@ public class Email  implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private UUID id;
 	
 	private String ownerRef;
