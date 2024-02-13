852042850a2501faa4505c109992b9d3e455cf0e
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-02-13 17:03:28 -0300

    Update EmailController.java
    - Criação dos metodos getAllEmails e getOneEmail;
    O primeiro metodo vai retornar todos os emails com paginação e o segundo um email via UUID;

--- ms-email/src/main/java/com/ms/email/controllers/EmailController.java
+++ ms-email/src/main/java/com/ms/email/controllers/EmailController.java
@@ -1,8 +1,16 @@
 package com.ms.email.controllers;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,4 +34,18 @@ public class EmailController {
 		return new ResponseEntity<>(email, HttpStatus.CREATED);
 	}
 	
+	@GetMapping("/emails")
+	public ResponseEntity<Page<Email>> getAllEmails(@PageableDefault(page = 0, size = 5, sort = "emailId", direction = Sort.Direction.DESC)Pageable pageable){
+		
+		return new ResponseEntity<>(service.findAll(pageable), HttpStatus.OK);
+	}
+	
+	@GetMapping("/emails/{id}")
+	public ResponseEntity<?>getOneEmail(@PathVariable(value = "emailId") UUID emailId){
+		Email email = service.findById(emailId);
+		if ( email != null) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Email not found");
+		}
+		return ResponseEntity.status(HttpStatus.OK).body(email);
+	}
 }
