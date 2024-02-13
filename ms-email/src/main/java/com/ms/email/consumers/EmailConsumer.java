1b3a7c78beb7f72c1cf2c7147d215025ea262fc6
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-02-13 11:43:54 -0300

    Create EmailConsumer.java
    - Criação da classe ouvinte da fila de mensageria;
    - Toda vez que chegar uma nova mensagem, o método listen irá ouvir, receber, montar o email e fazer a persistencia no banco de dados;

---
+++ ms-email/src/main/java/com/ms/email/consumers/EmailConsumer.java
@@ -0,0 +1,25 @@
+package com.ms.email.consumers;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+import com.ms.email.dtos.EmailDTO;
+import com.ms.email.models.Email;
+import com.ms.email.services.EmailService;
+
+@Component
+public class EmailConsumer {
+
+	@Autowired
+	EmailService service;
+	
+	@RabbitListener(queues = "${spring.rabbitmq.queue}")
+	public void listen(@Payload EmailDTO emailDTO) {
+		
+		Email emailModel = service.sendEmail(emailDTO);
+		System.out.println("Email Status: " + emailModel.getStatusEmail().toString());
+	}
+}
