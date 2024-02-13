e564defd05b1080d91a665b8791fa633d4e69712
Jefferson Pedro <jeffersonpedro05@gmail.com>
2024-02-12 22:22:09 -0300

    Update EmailService.java
    - Criando a lógica para o envio de email;

--- ms-email/src/main/java/com/ms/email/services/EmailService.java
+++ ms-email/src/main/java/com/ms/email/services/EmailService.java
@@ -1,10 +1,16 @@
 package com.ms.email.services;
 
+import java.time.LocalDateTime;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import com.ms.email.dtos.EmailDTO;
+import com.ms.email.enums.StatusEmail;
 import com.ms.email.models.Email;
 import com.ms.email.repositories.EmailRepository;
 
@@ -13,11 +19,35 @@ public class EmailService {
 	
 	@Autowired
 	EmailRepository repository;
+	
+	@Autowired
+	private JavaMailSender eMailSender;
 
 	public Email sendEmail(EmailDTO emailDTO) {
+		Email email = convertDTOToEmail(emailDTO);
+		email.setSendDateEmaiL(LocalDateTime.now());
 		
+		try {
+			SimpleMailMessage message = new SimpleMailMessage();
+			message.setFrom(email.getEmailFrom());
+			message.setTo(email.getEmailTo());
+			message.setSubject(email.getSubject());
+			message.setText(email.getText());
+			eMailSender.send(message);
+			
+			email.setStatusEmail(StatusEmail.SENT);
+		} catch (MailException e) {
+			email.setStatusEmail(StatusEmail.ERROR);
+		}finally {
+			return repository.save(email);
+		}
+		
+		
+	}
+	
+	private Email convertDTOToEmail(EmailDTO emailDTO) {
 		Email emailModel = new Email();
 		BeanUtils.copyProperties(emailDTO, emailModel);
-		return repository.save(emailModel);
+		return emailModel;
 	}
 }
